054aa862fe74623090b72d1ead88f2ceb51eb818
SwitchFun <48801768+SwitchFun@users.noreply.github.com>
2019-05-26 11:06:07 -0500

    Create api-insert.php

---
+++ apis/api-insert.php
@@ -0,0 +1,32 @@
+<?php
+
+//api-insert.php is for registering homebrew applications.
+
+$title = $_GET['TITLE'];
+$url = $_GET['URL'];
+$ver = "Public";
+$author = $_GET['UNKNOWN'];
+//Import SQL crap
+include "meekrodb.2.3.class.php";
+require_once 'meekrodb.2.3.class.php';
+//Define database information
+DB::$user = '';
+DB::$password = '';
+DB::$dbName = '';
+//Grab APP Keyword
+$appname = $_GET['app'];
+DB::insert('app', array(
+  'APP_TITLE' => $title,
+  'APP_VERSION' => "Public",
+  'APP_AUTHOR' => "Unknown",
+  'APP_URL' => $url
+));
+
+$TITLE = $query['APP_TITLE'];
+$VERSION = $query['APP_VERSION'];
+$AUTHOR = $query['APP_AUTHOR'];
+$URL = $query['APP_URL'];
+$finishedDataToOutput = JSON_Encode(['APP_URL' => "$URL"],JSON_UNESCAPED_SLASHES);
+echo $finishedDataToOutput;
+
+?>
