fc81725f761e41ab0733297d75ceb554eb6e21da
SwitchFun <48801768+SwitchFun@users.noreply.github.com>
2019-05-27 12:38:57 -0500

    Add support for author
    ^

--- apis/api-retrieve.php
+++ apis/api-retrieve.php
@@ -1,7 +1,4 @@
 <?php
-
-//API-Retrieve is the API that is used for retrieving APP_URL from the system.
-
 //Import SQL crap
 include "meekrodb.2.3.class.php";
 require_once 'meekrodb.2.3.class.php';
@@ -11,7 +8,7 @@ DB::$password = '';
 DB::$dbName = '';
 //Grab APP Keyword
 $appname = $_GET['app'];
-$query = DB::queryFirstRow("SELECT APP_URL from `app` where `APP_TITLE` = %s", $appname);
+$query = DB::queryFirstRow("SELECT APP_URL, APP_AUTHOR from `app` where `APP_TITLE` = %s", $appname);
 if(is_null($query)) {
     # we did not find any results at all.
     die(json_encode(['APP_URL' => 'Result not found!']));
@@ -21,7 +18,7 @@ $TITLE = $query['APP_TITLE'];
 $VERSION = $query['APP_VERSION'];
 $AUTHOR = $query['APP_AUTHOR'];
 $URL = $query['APP_URL'];
-$finishedDataToOutput = JSON_Encode(['APP_URL' => "$URL"],JSON_UNESCAPED_SLASHES);
+$finishedDataToOutput = JSON_Encode(['APP_URL' => "$URL", 'APP_AUTHOR' => "$AUTHOR"],JSON_UNESCAPED_SLASHES);
 echo $finishedDataToOutput;
 
 ?>
